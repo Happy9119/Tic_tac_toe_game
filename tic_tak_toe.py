9c30afde22d58082b58254505e6ad8dd813c4414
Happy Singh <kumar.sumit.9119@gmail.com>
2025-07-18 00:22:16 +0530

    Update tic_tak_toe.py

--- tic_tak_toe.py
+++ tic_tak_toe.py
@@ -1,220 +1,102 @@
-import os
-import random
+import tkinter as tk
+from tkinter import messagebox
 
-def display_row(r1, r2, r3):
-	os.system('clear')
-	print('*'*80)
-	print('\t\t\tWELCOME TO TICK TAC TOE GAME')
-	print('*'*80)
-	r1 = '	'.join(r1)
-	print(f'\n\t\t\t{r1}\n')
-	r2 = '	'.join(r2)
-	print(f'\t\t\t{r2}\n')
-	r3 = '	'.join(r3)
-	print(f'\t\t\t{r3}\n')
-	print('*'*80)
-	player_to_go_first()
-				
-def update_index1(z):
-	if (z in range(1,4)):
-		i = z-1
-		row1[i] = 'X'
-		display_row(row1, row2, row3)
-	if (z in range(4,7)):
-		i = z-3-1
-		row2[i] = 'X'
-		display_row(row1, row2, row3)
-	if (z in range(7,10)):
-		i = z-6-1
-		row3[i] = 'X'
-		display_row(row1, row2, row3)
-def update_index2(z):
-	if (z in range(1,4)):
-		i = z-1
-		row1[i] = 'O'
-		display_row(row1, row2, row3)
-	if (z in range(4,7)):
-		i = z-3-1
-		row2[i] = 'O'
-		display_row(row1, row2, row3)
-	if (z in range(7,10)):
-		i = z-6-1
-		row3[i] = 'O'
-		display_row(row1, row2, row3)
+class TicTacToeGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Tic Tac Toe Ultimate")
+        self.master.geometry("600x700")
+        self.master.resizable(False, False)
 
-def take_user_input(flag_passed, index_selected):
-	while(True):		
-		try:
-			if(flag_passed == 'A'):
-				index = int(input(f"Enter a Position to fill - Player A({player1_marker}) : 	"))
-				if (index in range(1, 10) and index not in index_selected):
-					return index
-				else:
-					print("Invalid Position given. Please select correct Values between 1 to 9")				
-					continue
-			else:
-				index = int(input(f"Enter a Position to fill - Player B({player2_marker}) : 	"))
-				if (index in range(1, 10) and index not in index_selected):
-					return index					
-				else:
-					print("Invalid Position given. Please select correct Values between 1 to 9")				
-					continue
-		except ValueError:
-			continue
+        # OPTIONAL: Set your own icon if you have one:
+        # self.master.iconbitmap('tictactoe.ico')
 
-def pass_index_to_matrix(index, m1, m2 ):
-	global flag
-	global index_selected
-	if (flag == 'A' and m1 == 'X'):
-		update_index1(index)
-		index_selected.append(index)
-		flag = 'B'
-	elif (flag == 'A' and m1 == 'O'):
-		update_index2(index)
-		index_selected.append(index)
-		flag = 'B'
-	elif (flag == 'B' and m2 == 'X'):
-		update_index1(index)
-		index_selected.append(index)
-		flag = 'A'
-	elif (flag == 'B' and m2 == 'O'):
-		update_index2(index)
-		index_selected.append(index)
-		flag = 'A'
-	
-def check_result():
-	for i in range(0,3):
-			for j in range(0,3):
-				for k in range(0,3):
-					if (row1[0] == row2[0] == row3[0] == 'X'):
-						counter1 = 3
-					elif (row1[1] == row2[1] == row3[1] == 'X'):
-						counter1 = 3
-					elif (row1[2] == row2[2] == row3[2] == 'X'):
-						counter1 = 3
-					elif (row1[0] == row1[1] == row1[2] == 'X'):
-						counter1 = 3
-					elif (row2[0] == row2[1] == row2[2] == 'X'):
-						counter1 = 3
-					elif (row3[0] == row3[1] == row3[2] == 'X'):
-						counter1 = 3
-					elif (row1[0] == row2[1] == row3[2] == 'X'):
-						counter1 = 3
-					elif (row1[2] == row2[1] == row3[0] == 'X'):
-						counter1 = 3
-					else:
-						counter1 = 0
-					if (counter1 == 3):
-						return counter1
-	for i in range(0,3):
-			for j in range(0,3):
-				for k in range(0,3):
-					if (row1[0] == row2[0] == row3[0] == 'O'):
-						counter2 = 3
-					elif (row1[1] == row2[1] == row3[1] == 'O'):
-						counter2 = 3
-					elif (row1[2] == row2[2] == row3[2] == 'O'):
-						counter2 = 3
-					elif (row1[0] == row1[1] == row1[2] == 'O'):
-						counter2 = 3
-					elif (row2[0] == row2[1] == row2[2] == 'O'):
-						counter2 = 3
-					elif (row3[0] == row3[1] == row3[2] == 'O'):
-						counter2 = 3
-					elif (row1[0] == row2[1] == row3[2] == 'O'):
-						counter2 = 3
-					elif (row1[2] == row2[1] == row3[0] == 'O'):
-						counter2 = 3
-					else:
-						counter2 = 0
-					if (counter2 == 3):
-						return counter2
+        self.player1_marker = tk.StringVar(value="X")
+        self.player2_marker = "O"
+        self.turn = "X"
+        self.board = ["*"] * 9
+        self.buttons = []
 
-def check_player():
-	temp = ['A' , 'B']
-	random.shuffle(temp)
-	z = temp[0]
-	return z
+        self.build_marker_selection()
 
-def player_marker():
-	while(True):
-		symbol = input('Player A: Please select a symbol (X or O)\n').upper()
-		if (symbol == 'X' or symbol == 'O'):
-			break
-		else:
-			print("Incorrect Symbol selected")
-			continue
-	if(symbol == 'X'):
-		sel = ('X', 'O')
-	else:
-		sel = ('O' , 'X')	
-	return sel
+    def build_marker_selection(self):
+        self.clear_window()
 
-def player_to_go_first():
-	global temp_flg
-	#m = player_marker()
-	if (temp_flg == 'A'):
-		print('Player A to go first')
-	elif (temp_flg == 'B'):
-		print('Player B to go first')
-	temp_flg = ''	
+        tk.Label(self.master, text="Choose Your Marker", font=("Arial", 24)).pack(pady=40)
+        frame = tk.Frame(self.master)
+        frame.pack()
 
-count = 0
-index_selected = []
+        tk.Radiobutton(frame, text="X", variable=self.player1_marker, value="X",
+                       font=("Arial", 20)).grid(row=0, column=0, padx=20)
+        tk.Radiobutton(frame, text="O", variable=self.player1_marker, value="O",
+                       font=("Arial", 20)).grid(row=0, column=1, padx=20)
 
+        tk.Button(self.master, text="Start Game", font=("Arial", 20),
+                  command=self.start_game).pack(pady=40)
 
+    def start_game(self):
+        self.player2_marker = "O" if self.player1_marker.get() == "X" else "X"
+        self.turn = "X"
+        self.board = ["*"] * 9
+        self.build_board()
 
-row1 = [ '*', '*', '*' ]
-row2 = [ '*', '*', '*' ]
-row3 = [ '*', '*', '*' ]
+    def build_board(self):
+        self.clear_window()
 
-flag = check_player()
-player1_marker, player2_marker = player_marker()
-temp_flg = flag
-display_row(row1, row2, row3)
+        self.status_label = tk.Label(self.master, text=f"{self.turn}'s Turn",
+                                     font=("Arial", 20))
+        self.status_label.pack(pady=20)
 
+        self.frame = tk.Frame(self.master)
+        self.frame.pack()
 
+        self.buttons = []
+        for i in range(9):
+            b = tk.Button(self.frame, text=" ", width=10, height=5,
+                          font=("Arial", 24, "bold"),
+                          command=lambda i=i: self.make_move(i),
+                          bg="#f0f0f0")
+            b.grid(row=i//3, column=i%3, padx=5, pady=5)
+            self.buttons.append(b)
 
-while(count < 9):
-	input_selected = take_user_input( flag, index_selected )
-	pass_index_to_matrix( input_selected , player1_marker, player2_marker )
-	matched = check_result()
-	if (matched == 3 and flag == 'B'):
-		print(f"\t\tThe Winner is : Player A({player1_marker}) ")
-		print('*'*80)
-		break
-	if (matched == 3 and flag == 'A'):
-		print(f"\t\tThe Winner is : Player B ({player2_marker}) ")
-		print('*'*80)		
-		break
-	else:
-		if (count == 8 and matched != 3):
-			print(f"\t\tMatch tied between A ({player1_marker}) and B ({player2_marker})")
-			print('*'*80)
-			break
-	count+=1
-	
+    def make_move(self, i):
+        if self.board[i] == "*":
+            marker = self.get_current_marker()
+            self.board[i] = marker
+            self.buttons[i].config(text=marker, disabledforeground="#000000")
+            self.buttons[i].config(state="disabled", bg="#d0f0c0" if marker == "X" else "#f0d0d0")
 
+            if self.check_winner(marker):
+                self.end_game(f"{marker} Wins!")
+            elif "*" not in self.board:
+                self.end_game("It's a Tie!")
+            else:
+                self.turn = "O" if self.turn == "X" else "X"
+                self.status_label.config(text=f"{self.turn}'s Turn")
 
+    def get_current_marker(self):
+        if self.turn == "X":
+            return "X" if self.player1_marker.get() == "X" else "O"
+        else:
+            return "O" if self.player1_marker.get() == "X" else "X"
 
+    def check_winner(self, marker):
+        combos = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ]
+        return any(all(self.board[i]==marker for i in combo) for combo in combos)
 
+    def end_game(self, result):
+        messagebox.showinfo("Game Over", result)
+        self.start_game()
 
+    def clear_window(self):
+        for widget in self.master.winfo_children():
+            widget.destroy()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TicTacToeGUI(root)
+    root.mainloop()
